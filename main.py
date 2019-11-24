4ddebba5ff719154e1945487fcddaa0a9cfb527c
Kevin De Angeli <kevindeangeli94@gmail.com>
2019-11-24 11:29:17 -0500

    Wrote script to read in the data and separate the labels

--- main.py
+++ main.py
@@ -7,13 +7,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sympy as sym
+import csv
+
+def readData(path):
+    file = open(path, "r")
+    Y = []
+    X = []
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        X.append(line[0:-3])
+        Y.append(line[-2])
+    Y = [int(i) for i in Y]  # make labels int instead of str
+
+    return np.array(X), np.array(Y)
+
+def getDataInfo(X, labels = False):
+    print("************************")
+    print("Example first 3 rows: \n")
+    print(X[0:3])
+    print("\nNumber of elements: ", len(X))
+    if labels:
+        print("Number of entries with label 1: ", np.sum(X==1))
+        print("Number of entries with label 0: ", len(X)-np.sum(X==1))
+    print("************************\n")
+
+
 
 
 def main():
-    f= '/Users/kevindeangeli/Desktop/Fall2019/COSC522/Final Project/Data/amazon_cells_labelled.txt'
-    print("main")
-    x = pd.read_csv(f, delim_whitespace=1, header=None)
-    print(X)
+    imdb= 'Data/imdb_labelled.txt'
+    amazon= 'Data/amazon_cells_labelled.txt'
+    yelp = 'Data/yelp_labelled.txt'
+
+    X, Y = readData(amazon)
+    getDataInfo(X)
+    getDataInfo(Y,True)
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
