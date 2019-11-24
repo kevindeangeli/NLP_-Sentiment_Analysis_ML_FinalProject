8338260ae1c043fa08c44bcec932474913c97d78
Kevin De Angeli <kevindeangeli94@gmail.com>
2019-11-24 10:26:52 -0500

    added the first python script

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Created by Kevin De Angeli
+Date: 2019-11-24
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sym
+
+
+def main():
+    f= '/Users/kevindeangeli/Desktop/Fall2019/COSC522/Final Project/Data/amazon_cells_labelled.txt'
+    print("main")
+    x = pd.read_csv(f, delim_whitespace=1, header=None)
+    print(X)
+
+if __name__ == "__main__":
+    main()
